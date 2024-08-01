5b9e86c2557ad9ef6f354dddb8b5b86be5572eaa
rauhja <82282638+rauhja@users.noreply.github.com>
2024-08-01 13:20:02 +0300

    healthkit manager documentation

--- Step Tracker/Managers/HealthKitManager.swift
+++ Step Tracker/Managers/HealthKitManager.swift
@@ -19,6 +19,8 @@ import Observation
     var weightData: [HealthMetric] = []
     var weightDiffData: [HealthMetric] = []
     
+    /// Fetch last 28 days of step count from HealthKit
+    /// - Returns: Array of ``HealthMetric``
     func fetchStepCount() async throws -> [HealthMetric] {
         guard store.authorizationStatus(for: HKQuantityType(.stepCount)) != .notDetermined else {
             throw STError.authNotDetermined
@@ -46,6 +48,9 @@ import Observation
         }
     }
     
+    /// Fetch most recent weight sample on each day for a specified number of days back from today.
+    /// - Parameter daysBack: Days back from today. Ex - 28 will return the last 28 days
+    /// - Returns: Array of ``HealthMetric``
     func fetchWeights(daysBack: Int) async throws -> [HealthMetric]{
         guard store.authorizationStatus(for: HKQuantityType(.bodyMass)) != .notDetermined else {
             throw STError.authNotDetermined
@@ -72,6 +77,10 @@ import Observation
         }
     }
     
+    /// Write step count data to HealthKit. Requires HealthKit write permission.
+    /// - Parameters:
+    ///   - date: Date for step count value
+    ///   - value: Step count value
     func addStepData(for date: Date, value: Double) async throws {
         
         let status = store.authorizationStatus(for: HKQuantityType(.stepCount))
@@ -95,6 +104,10 @@ import Observation
         }
     }
     
+    /// Write weight value to HealthKit. Requires HealthKit write permission.
+    /// - Parameters:
+    ///   - date: Date for weight value
+    ///   - value: Weight value in pounds. Uses pounds as a Double for .bodyMass conversions
     func addWeightData(for date: Date, value: Double) async throws {
         
         let status = store.authorizationStatus(for: HKQuantityType(.bodyMass))
@@ -118,6 +131,11 @@ import Observation
         }
     }
     
+    /// Creates a DateInterval between two dates
+    /// - Parameters:
+    ///   - date: End of date interval. Ex - today
+    ///   - daysBack: Start of date interval. Ex - 28 days ago
+    /// - Returns: Date range between two dates as a DateInterval
     private func createDateInterval(from date: Date, daysBack: Int) -> DateInterval {
         let calendar = Calendar.current
         let startOfEndDate = calendar.startOfDay(for: date)
