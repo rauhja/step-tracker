4911ccb0bdd9ec4856234d9f15da2cc36d781455
rauhja <82282638+rauhja@users.noreply.github.com>
2025-10-06 22:18:25 +0300

    New background gradient and chart background

--- Step Tracker/Charts/Charts/StepBarChart.swift
+++ Step Tracker/Charts/Charts/StepBarChart.swift
@@ -41,6 +41,7 @@ struct StepBarChart: View {
                         BarMark(x: .value("Date", steps.date, unit: .day),
                                 y: .value("Steps", steps.value)
                         )
+                        .cornerRadius(3)
                         .foregroundStyle(.pink.gradient)
                         .opacity(rawSelectedDate == nil || steps.date == selectedData?.date ? 1.0 : 0.3)
                     }
--- Step Tracker/Screens/HealthDataListView.swift
+++ Step Tracker/Screens/HealthDataListView.swift
@@ -23,6 +23,10 @@ struct HealthDataListView: View {
         metric == .steps ? hkData.stepData : hkData.weightData
     }
     
+    var backgroundColor: Color {
+        metric == .steps ? .pink : .indigo
+    }
+    
     var body: some View {
         List(listData.reversed()) { data in
             LabeledContent {
@@ -31,9 +35,15 @@ struct HealthDataListView: View {
                 Text(data.date, format: .dateTime.month().day().year())
                     .accessibilityLabel(data.date.accessibilityDate)
             }
+            .listRowBackground(Color(.secondarySystemBackground).opacity(0.35))
             .accessibilityElement(children: .combine)
         }
         .navigationTitle(metric.title)
+        .scrollContentBackground(.hidden)
+        .background(LinearGradient(colors: [backgroundColor.opacity(0.25), .clear],
+                                   startPoint: .topLeading,
+                                   endPoint: .bottomTrailing)
+        )
         .sheet(isPresented: $isShowingAddData) {
             addDataView
         }
